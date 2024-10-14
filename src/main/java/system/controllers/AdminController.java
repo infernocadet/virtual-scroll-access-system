e2947d551e0a95d37e63f169c64c5fd872b29f79
Edward Khuu <edward@Edwards-MacBook-Air.local>
2024-10-14 22:44:03 +1100

    admins can view scroll statistics

--- src/main/java/system/controllers/AdminController.java
+++ src/main/java/system/controllers/AdminController.java
@@ -3,9 +3,11 @@ package system.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import system.models.Scroll;
 import system.models.User;
 import system.repositories.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import system.services.ScrollService;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -15,10 +17,12 @@ public class AdminController {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final ScrollService scrollService;
 
-    public AdminController(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+    public AdminController(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder, ScrollService scrollService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.scrollService = scrollService;
     }
 
     // View all users
@@ -29,6 +33,14 @@ public class AdminController {
         model.addAttribute("newUser", new User()); // Add an empty User object for the add form
         return "admin/view_users";
     }
+
+    @GetMapping("/admin/statistics")
+    public String viewAllScrolls(Model model) {
+        List<Scroll> scrolls = scrollService.findAll();
+        model.addAttribute("scrolls", scrolls);
+        return "admin/view_scrolls";
+    }
+
 
     // Add a new user
     @PostMapping("/admin/users/add")
@@ -47,3 +59,4 @@ public class AdminController {
         return "redirect:/admin/users";
     }
 }
+
