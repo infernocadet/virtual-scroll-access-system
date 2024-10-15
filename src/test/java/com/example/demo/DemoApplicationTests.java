2a1f4e5ca41fd9110a5c2b68007cdb43ecbb8ce0
Marc Caramoan <caramoanmarc@gmail.com>
2024-10-15 12:11:10 +1100

    fixed initialistion test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,8 +2,9 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import system.Application;
 
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 class DemoApplicationTests {
 
 	@Test
