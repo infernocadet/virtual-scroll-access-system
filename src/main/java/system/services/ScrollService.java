1684b55b9a2ab9df37f6c7e95878ec1e0c06be2a
Marc Caramoan <caramoanmarc@gmail.com>
2024-10-15 11:47:50 +1100

    #24 - showing scroll date and time formatted

--- src/main/java/system/services/ScrollService.java
+++ src/main/java/system/services/ScrollService.java
@@ -1,11 +1,13 @@
 package system.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 import system.models.Scroll;
 import system.repositories.ScrollRepository;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,24 +16,42 @@ import java.util.Optional;
 public class ScrollService {
 
     private final ScrollRepository scrollRepository;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy - hh:mm a");
 
     public boolean nameExists(String name) {
         return scrollRepository.findByNameIgnoreCase(name).isPresent();
     }
 
     public Optional<Scroll> findById(int id) {
-        return scrollRepository.findById(id);
+        return scrollRepository.findById(id).map(this::formatScrollDates);
     }
 
     public List<Scroll> findAll() {
-        return scrollRepository.findAll();
+        List<Scroll> scrolls = scrollRepository.findAll();
+        scrolls.forEach(this::formatScrollDates);
+        return scrolls;
     }
 
     public Scroll save(Scroll scroll) {
         if (scroll.getId() == 0) {
+            // for new scroll
             scroll.setCreatedAt(LocalDateTime.now());
+            scroll.setUpdatedAt(LocalDateTime.now());
+        } else {
+            // if scroll exists but being updated
+            scroll.setUpdatedAt(LocalDateTime.now());
         }
         return scrollRepository.save(scroll);
+    }
+
+    private Scroll formatScrollDates(Scroll scroll){
+        if (scroll.getCreatedAt() != null){
+            scroll.setFormattedCreatedAt(scroll.getCreatedAt().format(formatter));
+        }
+        if (scroll.getUpdatedAt() != null){
+            scroll.setFormattedUpdatedAt(scroll.getUpdatedAt().format(formatter));
+        }
+        return scroll;
     }
 
     public void delete(Scroll scroll) {
