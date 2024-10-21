800dfd2e004fe56e7720e63c4220bd486670fef5
Marc Caramoan <caramoanmarc@gmail.com>
2024-10-21 15:38:28 +1100

    search using userID, scrollID, scroll name, dates

--- src/main/java/system/repositories/ScrollRepository.java
+++ src/main/java/system/repositories/ScrollRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import system.models.Scroll;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,5 +16,9 @@ public interface ScrollRepository extends JpaRepository<Scroll, Integer> {
     If ur query may return mutliple thigns
      */
     List<Scroll> findByNameContainingIgnoreCase(String name);
+    List<Scroll> findByUserId(int userId); // can give multiple
+    Optional<Scroll> findById(int scrollId); // will only give one scroll
+    List<Scroll> findByCreatedAtAfter(LocalDateTime date);
+    List<Scroll> findByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate);
 
 }
--- src/main/java/system/services/ScrollService.java
+++ src/main/java/system/services/ScrollService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import system.models.Scroll;
 import system.repositories.ScrollRepository;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -35,6 +36,37 @@ public class ScrollService {
 
     public List<Scroll> findByName(String name){
         return scrollRepository.findByNameContainingIgnoreCase(name);
+    }
+
+    public List<Scroll> findByUserID(int userId){
+        return scrollRepository.findByUserId(userId);
+    }
+
+    public List<Scroll> findByCreatedAtAfter(LocalDateTime date){
+        return scrollRepository.findByCreatedAtAfter(date);
+    }
+
+    public List<Scroll> findByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate){
+        return scrollRepository.findByCreatedAtBetween(startDate, endDate);
+    }
+
+    public List<Scroll> searchScrolls(Integer userId, Integer scrollId, String name, LocalDateTime startDate, LocalDateTime endDate) {
+
+        if (scrollId != null){
+            return scrollRepository.findById(scrollId).stream().toList();
+        }
+
+        List<Scroll> result = new ArrayList<>();
+
+        if (userId != null) {
+            result = scrollRepository.findByUserId(userId);
+        } else if (name != null && !name.isEmpty()) {
+            result = scrollRepository.findByNameContainingIgnoreCase(name);
+        } else if (startDate != null && endDate != null) {
+            result = scrollRepository.findByCreatedAtBetween(startDate, endDate);
+        }
+
+        return result;
     }
 
     public Scroll save(Scroll scroll) {
